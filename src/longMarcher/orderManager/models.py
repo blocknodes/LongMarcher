d4f181a4b757678f0f0bb9118cc745c1628acac8
Your Name <you@example.com>
2020-07-21 11:02:21 +0100

    Add order model

--- src/longMarcher/orderManager/models.py
+++ src/longMarcher/orderManager/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from picklefield.fields import PickledOjbectField
 
 # Create your models here.
+
+class Order(models.Model):
+    phone = models.CharField(max_length=12)
+    customer = models.CharField()
+    create_at = models.DateField(auto_now_add=True)
+    deliver_at = models.DateField()
+    price = models.DecimalField(decimal_places=2)
+    pre_paid = models.DecimalField(decimal_places=2)
+    extra_info = PickledOjbectField()
