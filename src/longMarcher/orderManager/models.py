b7142df24aff51ac1acd939edb10df3d5e0c73be
Your Name <you@example.com>
2020-07-21 12:45:07 +0100

    Add model to admin site

--- src/longMarcher/orderManager/models.py
+++ src/longMarcher/orderManager/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from picklefield.fields import PickledOjbectField
+from picklefield.fields import PickledObjectField
 
 # Create your models here.
 
 class Order(models.Model):
     phone = models.CharField(max_length=12)
-    customer = models.CharField()
+    customer = models.CharField(max_length=100)
     create_at = models.DateField(auto_now_add=True)
     deliver_at = models.DateField()
-    price = models.DecimalField(decimal_places=2)
-    pre_paid = models.DecimalField(decimal_places=2)
-    extra_info = PickledOjbectField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    pre_paid = models.DecimalField(max_digits=10, decimal_places=2)
+    extra_info = PickledObjectField()
